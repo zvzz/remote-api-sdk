af7e83c37d580476749cf66fb170cce056892d11
mihail <mihail@mihails-MacBook-Pro.local>
2015-01-21 12:39:26 +0700

    Test for explaining API

--- api_test.php
+++ api_test.php
@@ -3,68 +3,257 @@
 require_once('api_model.php');
 require_once('api_controller.php');
 
-$signature1 = sha1('getStationsList' . API_KEY . '1');
-$signature2 = sha1('getRouteList' . API_KEY . '2');
-$signature3 = sha1('getSchedule' . API_KEY . '3');
+/**
+ * Validate response getBookingDetails
+ * throw exception if error found
+ * @param Array $res
+ * @return null
+ */
+function validateBookingDetails($res)
+{
+	$fields = array('id', 'satus', 'from', 'to', 'class', 'date', 'time', 'email', 'phone');
+	if (!isset($res['result']) or !is_array(($res['result']))) {
+		throw new Exception("Result not found in " . json_encode($res));
+	}
+	$res = $res['result'];
+	foreach ($fields as $field) {
+		if (!isset($res[$field])) {
+			throw new Exception(" '{$field}' not found in " . json_encode($res));		
+		}
+	}
+	if (!isset($res['passengers']) or !is_array(($res['passengers']))) {
+		throw new Exception("Passengers information not found in " . json_encode($res));
+	}
+	if (!validatePassengers($res['passengers'])) {
+		throw new Exception("Passengers information not valid in " . json_encode($res['passengers']));
+	}
+}
 
-//exit();
+/**
+ * Validate response for booking methods
+ * throw exception if error found
+ * @param Array $response
+ * @return null
+ */
+function validateSuccessful(Array $res)
+{
+	if (!isset($res['result']) or !is_array(($res['result']))) {
+		throw new Exception("Result not found in " . json_encode($res));
+	}
+	if (!isset($res['result']['successful'])) {
+		throw new Exception("Successful flag not found in " . json_encode($res));
+	}
+}
 
-var_dump(validateParamValue(API_PARAM_PHONE, '123'));
-var_dump(validateParamValue(API_PARAM_PHONE, '123a'));
-var_dump(validateParamValue(API_PARAM_PHONE, '+66123123'));
-var_dump(validateParamValue(API_PARAM_PHONE, '1231244123'));
+/**
+ * Validate getStationsList response structure
+ * throw exception if error found
+ * @param Array $list of station
+ * @return null
+ */
+function validateStationsList(Array $list)
+{
+	foreach ($list as $station) {
+		if (isset($station['id']) and isset($station['name'])) {
+			continue;
+		}
+		throw new Exception("Invalid station structure: " . json_encode($station));
+	}
+}
 
+/**
+ * Validate getRoutesList response structure
+ * throw exception if error found
+ * @param Array $list of routes
+ * @return null
+ */
+function validateRoutesList(Array $list)
+{
+	$fields = array('class', 'departures', 'route', 'price');
+	foreach ($list as $route) {
+		foreach ($fields as $field) {
+			if (!isset($route[$field])) {
+				throw new Exception("Invalid route structure");	
+			}
+			if ($field == 'class') {
+				continue;
+			}
+			if (!is_array($route[$field]) or count($route[$field]) === 0) {
+				throw new Exception("Invalid route '{$field}' value");
+			}
+		}
+	}
+}
 
-var_dump(validateParamValue(API_PARAM_EMAIL, 'aad@ddd'));
-var_dump(validateParamValue(API_PARAM_EMAIL, 'asd@asdd.12'));
-var_dump(validateParamValue(API_PARAM_EMAIL, 'mail@test.dom'));
-var_dump(validateParamValue(API_PARAM_EMAIL, 'mail@test.ru'));
+/**
+ * Validate getSeatsMap response structure
+ * throw exception if error found
+ * @param Array $map seats map layout
+ * @return null
+ */
+function validateSeatsMap(Array $map) 
+{
+	foreach ($map as $id => $floor) {
+		foreach(array('rows', 'layout', 'custom', 'booked') as $field) {
+			if (!isset($floor[$field])) {
+				throw new Exception("Invalid foor structure: " . json_encode($floor));
+			}
+		}		
+	}
+}
 
+/**
+ * Validate getScheduler response structure
+ * throw exception if error found
+ * @param Array $map list of trips
+ * @return null
+ */
+function validateSchedule(Array $list) 
+{
+	foreach ($list as $trip) {
+		foreach(array('time', 'class', 'price', 'seats') as $field) {
+			if (!isset($trip[$field])) {
+				throw new Exception("Invalud schedule structure");
+			}
+		}
+	}
+	
+}
 
-var_dump(validateParamValue(API_PARAM_TIME, '10:10'));
-var_dump(validateParamValue(API_PARAM_TIME, '12:10'));
-var_dump(validateParamValue(API_PARAM_TIME, '24:10'));
-var_dump(validateParamValue(API_PARAM_TIME, '10:61'));
+foreach (array('123', '123a', 's123a') as $value) {
+	if (validateParamValue(API_PARAM_PHONE, $value)) {
+		throw new Exception("Invalid phone validation result for {$value}");
+	}
+}
+foreach (array('+66123123', '1231244123', '+78083124423') as $value) {
+	if (!validateParamValue(API_PARAM_PHONE, $value)) {
+		throw new Exception("Invalid phone validation result for {$value}");
+	}
+}
 
+foreach (array('aad@ddd', 'asd@asdd.12', 'asdwedds') as $value) {
+	if (validateParamValue(API_PARAM_EMAIL, $value)) {
+		throw new Exception("Invalid email validation result for {$value}");
+	}
+}
+foreach (array('me@test.com', 'book@gmail.com', 'valid@email.com') as $value) {
+	if (!validateParamValue(API_PARAM_EMAIL, $value)) {
+		throw new Exception("Invalid email validation result for {$value}");
+	}	
+}
 
-var_dump(validateParamValue(API_PARAM_DATE, '2015-10-10'));
-var_dump(validateParamValue(API_PARAM_DATE, '2015-12-12'));
-var_dump(validateParamValue(API_PARAM_DATE, '2015-10-33'));
-var_dump(validateParamValue(API_PARAM_DATE, '2015-13-10'));
+foreach (array('09:00', '10:00', '12:40', '22:10') as $value) {
+	if (!validateParamValue(API_PARAM_TIME, $value)) {
+		throw new Exception("Invalid time validation result for {$value}");
+	}
+}
+foreach (array('2:13', '11:3', '12:61', '25:40') as $value) {
+	if (validateParamValue(API_PARAM_TIME, $value)) {
+		throw new Exception("Invalid time validation result for {$value}");
+	}	
+}
 
+foreach (array('2015-10-10', '2015-12-10', '2015-10-20', '2015-05-05') as $value) {
+	if (!validateParamValue(API_PARAM_DATE, $value)) {
+		throw new Exception("Invalid date validation result for {$value}");
+	}
+}
+foreach (array('10-10-1999', '2015-15-15', '15/10/10', '15/20/05') as $value) {
+	if (validateParamValue(API_PARAM_DATE, $value)) {
+		throw new Exception("Invalid time validation result for {$value}");
+	}	
+}
 
-var_dump(callApiRequest(array(
-	'method' => 'getStationsList', 
-	'code'   => '1',
-	'signature' => $signature1
-))); 
+$code = mt_rand(1, 10000);
+$res  = callApiRequest(array(
+	'method'    => 'getStationsList', 
+	'code'      => $code,
+	'signature' => sha1('getStationsList' . API_KEY . $code),
+)); 
 
-var_dump(callApiRequest(array(
-	'method'  => 'getRouteList', 
-	'from_id' => 1,
-	'code'    => '2',
-	'signature' => $signature2
-)));
+validateStationsList($res['result']);
 
-var_dump(callApiRequest(array(
+$code = mt_rand(1, 10000);
+$res  = callApiRequest(array(
+	'method'    => 'getRoutesList', 
+	'code'      => $code,
+	'signature' => sha1('getRoutesList' . API_KEY . $code)
+));
+
+validateRoutesList($res['result']);
+
+$code = mt_rand(1, 10000);
+$res  = callApiRequest(array(
 	'method'  => 'getSchedule', 
-	'from_id' => 1,
-	'to_id'   => '1',
+	'from_id' => 9,
+	'to_id'   => 13,
 	'date'    => '2015-12-02', 
-	'code'    => '3',
-	'signature' => $signature3
-)));
+	'code'    => $code,
+	'signature' => sha1('getSchedule' . API_KEY . $code)
+));
 
-var_dump(callApiRequest(array('method' => 'getSeatsMap', 'from_id' => 1, 'to_id' => '1', 'date' => '2015-12-02', 'time' => '10:20', 'bus_type' => ' a sd1 2dd s2')));
-var_dump(callApiRequest(array(
-	'method'   => 'reservSeats', 
-	'from_id'  => 1, 
-	'date'     => '2015-12-02',
-	'time'     => '10:20',
-	'to_id'    => '1123',
-	'bus_type' => ' a sd1 2dd s2',
-	'passengers' => json_encode(array(array('first_name' => 'John', 'second_name' => 'Smith'),array('first_name' => 'Bob', 'second_name' => 'Chop'))),
-)));
-var_dump(callApiRequest(array('method' => 'confirmBooking', 'booking_id' => 'h247b45c20')));
-var_dump(callApiRequest(array('method' => 'cancelBooking', 'booking_id' => 'h247b45c20')));
-var_dump(callApiRequest(array('method' => 'getBookingDetail', 'booking_id' => 'h247b45c20')));
+validateSchedule($res['result']);
+
+$code = mt_rand(1, 10000);
+$res  = callApiRequest(array(
+	'method'    => 'getSeatsMap',
+	'from_id'   => 9, 
+	'to_id'     => 13, 
+	'date'      => '2015-12-02', 
+	'time'      => '11:00', 
+	'class'     => 'VIP',
+	'code'      => $code,
+	'signature' => sha1('getSeatsMap' . API_KEY . $code)
+));
+
+validateSeatsMap($res['result']);
+
+$code = mt_rand(1, 10000);
+$res  = callApiRequest(array(
+	'method'     => 'reserveSeats', 
+	'from_id'    => 1, 
+	'date'       => '2015-12-02',
+	'time'       => '11:00',
+	'to_id'      => 13,
+	'class'      => 'VIP',
+	'code'       => $code,
+	'email'		 => 'bob@mail.com',
+	'phone'		 => '+669001002020',
+	'signature'  => sha1('reserveSeats' . API_KEY . $code),
+	'passengers' => json_encode(array(
+		array('first_name' => 'John', 'last_name' => 'Smith', 'seat' => '1C'),
+		array('first_name' => 'Bob',  'last_name' => 'Chop',  'seat' => '2C')
+	)),
+));
+
+validateSuccessful($res);
+
+$code = mt_rand(1, 10000);
+$res  = callApiRequest(array(
+	'method'     => 'confirmBooking',
+	'booking_id' => 'h247b45c20',
+	'code'       => $code,
+	'signature'  => sha1('confirmBooking' . API_KEY . $code),
+));
+
+validateSuccessful($res);
+
+$code = mt_rand(1, 10000);
+$res  = callApiRequest(array(
+	'method'     => 'cancelBooking', 
+	'booking_id' => 'h247b45c20', 
+	'code'       => $code, 
+	'signature'  => sha1('cancelBooking' . API_KEY . $code),
+));
+
+validateSuccessful($res);
+
+$code = mt_rand(1, 10000);
+$res  = callApiRequest(array(
+	'method'     => 'getBookingDetail', 
+	'booking_id' => 'h247b45c20',
+	'code'       => $code, 
+	'signature'  => sha1('getBookingDetail' . API_KEY . $code),
+));
+
+validateBookingDetails($res);
